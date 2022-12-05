0b282360045f35d5426ee1e56d6d707bb56aa3eb
BWNC-code <bryn.wheeler@gmail.com>
2022-12-05 17:09:44 +0000

    Add splash image to homepage

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function myFunction() {
       x.className = "nav";
     }
   }
+
+  const splash = document.querySelector('.splash');
+
+  document.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{
+      splash.classList.add('display-none');
+    }, 2000);
+  })
